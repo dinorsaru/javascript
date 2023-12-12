0e894b5bc6545b3924ce98ce4ad26f1a0e71d52b
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-12 16:40:38 -0300

    aula 11

---
+++ aula11/aula08.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){
+    console.log('voce ultrapassou o limite de velocidade, voce foi MULTADO!')
+}
+console.log('dirija sempre com velocidade')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'BRA'){
+    console.log('brasileiro')
+}else{
+    console.log('estrangeiro')
+}
