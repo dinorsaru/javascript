ab5d3849037a28579d9de66be55fea2cf561f5c6
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-15 10:43:30 -0300

    mais ambiente e vetores

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -2,3 +2,12 @@ let num = [5,8,2,9,3]
 num.push(4)
 num.sort()
 console.log(`Nosso vetor é o ${num}`)
+
+for (let pos=0;pos<num.length;pos++){
+    console.log(num[pos])
+    
+}
+
+/*let valo = [8,1,7,4,2,9]
+for(let pos=0; pos < valo.length; pos++)
+    console.log(`a posição ${pos} tem o valor ${valo[pos]}`)*/
---
+++ aula15/variaveltela.js
@@ -0,0 +1,5 @@
+let valo = [8,1,7,4,6,9,3,4,5]
+
+for (let pos in valo){
+    console.log(`a posição ${pos} tem o valor ${valo[pos]}`)
+}
