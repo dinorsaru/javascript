5d45a202e4006de492267d324fb49bbad6598cf4
Antony Guilherme <83239872+dinorsaru@users.noreply.github.com>
2023-12-18 13:50:00 -0300

    finish

--- aula16ex/scrpt02.js
+++ aula16ex/scrpt02.js
@@ -21,9 +21,34 @@ function inlista(n, l){
 
 function adicionar(){
     if (isNumero(num.value) && !inlista(num.value, valores)){
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else{
         window.alert(' valor invalido ou ja encontrado')
     }
+    num.value = ''
+    num.focus()
+}
 
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let total = valores.length
+        let maior = valores [0]
+        let menor = valores [0]
+        for(let pos in valores){
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p> ao todo temos ${total} nuemros cadastrados. </p>`
+        res.innerHTML += `<p> o maior valor informado foi ${maior}. </p>`
+        res.innerHTML += `<p> o menor valor informado foi ${menor}. </p>`
+    }
 }
